dae6271be02cd66674127c567bcb0ad482a2384b
thangpham20521903 <78943642+thangpham20521903@users.noreply.github.com>
2023-12-08 16:05:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle as pkl
 import numpy as np
 
-class_list = {'2': 'Positive', '0': 'Negative', '0': 'Normal'}
+class_list = {'2': 'Positive', '0': 'Negative', '1': 'Normal'}
 
 st.title('Emotional Prediction')
 input = open('ec_vsfc.pkl', 'rb')
